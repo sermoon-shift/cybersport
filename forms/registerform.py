3005444499c32e0453a47a63dc2191100d3b7da2
alena <al.3r44@yandex.ru>
2026-04-14 20:05:11 +0300

    2 commit

---
+++ forms/registerform.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, EmailField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Войти')
