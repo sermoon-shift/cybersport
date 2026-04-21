41a80dd97c817e4c417c89efcfc1c0b61b54cfd1
sermoon-shift <serzacev60@gmail.com>
2026-04-21 18:26:57 +0300

    Commit with tournaments

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ def index():
 def news():
     return render_template("news.html")
 
+@app.route("/games")
+def games():
+    return render_template("tournament.html")
+
+@app.route("/register_ontournament")
+def register_ontournament():
+    return render_template("register_ontournament.html")
+
+@app.route("/join_tournament")
+def join():
+    return render_template("join_tournament.html")
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
